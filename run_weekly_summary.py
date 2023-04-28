a13165d3829f3c0794bdb5387813e50fc190d03b
Rohan Mathur <rmathur101@gmail.com>
2023-04-28 15:05:12 -0500

    cleanup; add before arg; check if file already exists for date range before creation while ignoring the creation timestamp

--- run_weekly_summary.py
+++ run_weekly_summary.py
@@ -8,6 +8,7 @@ from transformers import GPT2TokenizerFast
 from datetime import datetime, date, timedelta
 import subprocess
 from pathlib import Path
+import re
 load_dotenv()
 
 # constants 
@@ -42,22 +43,40 @@ CHANNEL_AND_THREAD_IDS = {
 }
 
 # get date a week ago from today
-def get_export_after_str():
-    today = date.today()
-    last_week = today - timedelta(days=7)
+def get_one_week_before_ref_date(reference_date=None):
+    # if custom date str get timedelta from that
+    if reference_date:
+        custom_date = datetime.strptime(reference_date, '%Y-%m-%d')
+    else:
+        custom_date = date.today()
+
+    last_week = custom_date - timedelta(days=7)
     last_week_str = last_week.strftime('%Y-%m-%d')
     return last_week_str
 
 # get today's date as string
 def get_today_str():
-    today = date.today()
-    today_str = today.strftime('%Y-%m-%d')
-    return today_str
+    return date.today().strftime('%Y-%m-%d')
 
 def check_file_exists(file_path):
     return os.path.isfile(file_path)
 
-def gen_and_get_discord_export(export_path, discord_token, channel_key, output_type, after, before=None, force_file_regen=False):
+# if file matches the following we don't want to recreate it, note that it's ok if the files were created on different dates (i.e. created_on doesn't have to match)
+def check_matching_file_exists_ignore_creation_date(dir, file_name):
+    pattern = r"_created_on_\d{4}-\d{2}-\d{2}"
+    file_name_adjusted = re.sub(pattern, "", file_name)
+
+    for check_file in os.listdir(dir):
+        check_file_adjusted = re.sub(pattern, "", check_file)
+        if check_file_adjusted == file_name_adjusted:
+            return True
+
+    return False
+
+def gen_and_get_discord_export(export_path, discord_token, channel_key, output_type, after, before, force_file_regen=False):
+    # print arguments
+    print(f"\nGEN_AND_GET_DISCORD_EXPORT:\nexport_path: {export_path}\nchannel_key: {channel_key}\noutput_type: {output_type}\nafter: {after}\nbefore: {before}\nforce_file_regen: {force_file_regen}\n")
+
     # get channel id
     channel_id = CHANNEL_AND_THREAD_IDS[channel_key]['id']
 
@@ -65,16 +84,16 @@ def gen_and_get_discord_export(export_path, discord_token, channel_key, output_t
     output_ext = 'html' if output_type == 'htmldark' else 'json'
 
     # build output file
-    output_file = f"export_{channel_key}_after_{after}_created_on_{get_today_str()}.{output_ext}"
+    output_file = f"export_{channel_key}_after_{after}_before_{before}_created_on_{get_today_str()}.{output_ext}"
 
     # if file exists and not force_file_regen, return
     file_path = DISCORD_EXPORT_DIR_PATH_RAW + '/' + output_file 
-    if (check_file_exists(file_path) and not force_file_regen):
+    if (check_matching_file_exists_ignore_creation_date(DISCORD_EXPORT_DIR_PATH_RAW, output_file) and not force_file_regen):
         print(f"\nFILE EXISTS: {file_path}\n")
         return output_file, output_type
 
     # generate discord export
-    docker_command = f"docker run --rm -v {export_path}:/out tyrrrz/discordchatexporter:stable export -t {discord_token} -c {channel_id} -f {output_type} -o {output_file} --after {after}"
+    docker_command = f"docker run --rm -v {export_path}:/out tyrrrz/discordchatexporter:stable export -t {discord_token} -c {channel_id} -f {output_type} -o {output_file} --after {after} --before {before}"
     print(f"\nCREATING NEW FILE! FORCED: {force_file_regen}. DOCKER COMMAND:\n{docker_command}\n")
     subprocess.run(docker_command, shell=True)
 
@@ -85,8 +104,22 @@ channel_key_to_message_data = {}
 
 # gen and load discord exports for all channels
 for channel_key in CHANNEL_AND_THREAD_IDS:
+    #TODO this should be today's date when you want to generate a weekly summary; in fact you may need to do today's date + 1 - need to check this 
+    reference_date = '2023-04-27'
+
     # create discord export
-    file_name, file_type = gen_and_get_discord_export(DISCORD_EXPORT_DIR_PATH, DISCORD_TOKEN_ID, channel_key, 'json', get_export_after_str(), None, False)
+    file_name, file_type = gen_and_get_discord_export(
+        DISCORD_EXPORT_DIR_PATH, 
+        DISCORD_TOKEN_ID, 
+        channel_key, 
+        'json', 
+        get_one_week_before_ref_date(reference_date='2023-04-27'), 
+        '2023-04-27', 
+        False
+    )
+
+    # end program (just for testing)
+    sys.exit()
 
     # read file as json if json
     if file_type == 'json':
