b61aed618dff8fc5cad109e863af1b3a048c4ede
Rohan Mathur <rmathur101@gmail.com>
2023-05-10 14:57:50 -0500

    new constants file

---
+++ constants.py
@@ -0,0 +1,39 @@
+# discord constants 
+DISCORD_EXPORT_DIR_PATH = os.getenv('DISCORD_EXPORT_DIR_PATH') 
+DISCORD_EXPORT_DIR_PATH_RAW = os.getenv('DISCORD_EXPORT_DIR_PATH_RAW') 
+DISCORD_TOKEN_ID = os.getenv('DISCORD_TOKEN_ID')
+
+# openai constants
+COMPLETIONS_MODEL = os.getenv("COMPLETIONS_MODEL")
+ENCODING = tiktoken.get_encoding("cl100k_base")
+
+# set my API key
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# relevant channels
+CHANNEL_AND_THREAD_IDS = {
+    'lectures': {
+        'id': '902967453183778836',
+    },
+    'staying_ahead_ai_accel': {
+        'id': '1088550425524961360'
+    },
+    'proof_of_building': {
+        'id': '1086919421790015629'
+    },
+    'proof_of_workout': {
+        'id': '915652523677868042'
+    },
+    'educational_contents_for_builders': {
+        'id': '1043848628097261658'
+    },
+    'proof_of_learning': {
+        'id': '919209947072434176'
+    },
+    'building_wealth_and_sharing_alpha': {
+        'id': '950184868837490748'
+    },
+    'tns_nostr': {
+        'id': '1073100882700406824'
+    }
+}
